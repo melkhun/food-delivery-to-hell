74926b04ecbeb5e98e94befc53fccf22c8467a5e
fernelo <ferne.lo.2017@sis.smu.edu.sg>
2019-04-01 01:30:22 +0800

    snek

--- list-view.php
+++ list-view.php
@@ -1,20 +1,39 @@
 <html>
-<title>G5T1-Restaurant</title>
-<body>
-    <center>
 
-    <h1>G5T1-Retstraunt</h1>
-    <div class="col-md-6">
-    <form id="Pay" class="form" action="payment.php" method="post"> 
-        <table id="booksTable" class='table table-striped' style="font-size:40px" id='book-list' border='1'>
-            <tr>
-                <th>Item</th>
-                <th>Price</th>
-                <th>Discription</th>
-                <th>Quantity</th>
-            </tr>
-        </table>
+
+<head>
+    <title>G5T1-Restaurant</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+</head>
+
+
+<body>
+
+    <div class="container">
+        <div class="jumbotron text-center">
+            <img src="snaktime_logo.png" class="img-fluid">
+            <form id="Pay" class="form" action="payment.php" method="post"> 
+                <table id="booksTable" class='table text-center' id='book-list'>
+                    <tr>
+                        <th>Item</th>
+                        <th>Price</th>
+                        <th>Description</th>
+                        <th>Quantity</th>
+                    </tr>
+                </table>
+                <button name="pay" type="submit" class="btn btn btn-success center-block" style="font-size : 30px; height: 100px; width:300px">Confirm Order &rarr; </button>
+            </form>
+        </div>
     </div>
+
+</body>
+    
+            
+
+
 <script>
     $(function () {
         // Change serviceURL to your own
@@ -39,14 +58,6 @@
     });
 </script>
 
-    <button name="pay" type="submit" class="btn btn-primary center-block" style="font-size : 30px; height: 100px; width:300px">Confirm Order &rarr; </button>
-     
-    </form>
-</center>
-</body>
-    
-            
 </html>
-
 
 
--- track.php
+++ track.php
@@ -2,22 +2,26 @@
 <html>
 <head>
     <title>G5T1-Restaurant</title>
-    <script
-        src="https://code.jquery.com/jquery-3.3.1.js"
-        integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
-        crossorigin="anonymous">
-    </script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <!-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script> -->
 </head>
+
 <body>
 
-    <h1>G5T1-Retstraunt</h1>
-    <div class="col-md-6">
-        <p id="eta">Arriving in: </p>
+    <div class="container">
+        <div class="jumbotron text-center">
+            <img src="snaktime_logo.png" class="img-fluid">
+            <p class="lead ">Arriving in...</p>
+                <p id="eta"> </p>
+        </div>
     </div>
-<script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.js"></script>
+
+    <script>
     $(function () {
         // Change serviceURL to your own
-        var serviceURL = "http://DESKTOP-VHQ8QDK:8082/track/1";
+        var serviceURL = "http://DESKTOP-VHQ8QDK:8080/track/1";
         var eta = "";
         $.get(serviceURL, function (data) {
             var json = JSON.parse(data);
@@ -28,6 +32,10 @@
 </script>
 
 </body>
+    
+
+
+
     
 </html>
 
