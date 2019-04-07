01b3bb0035a5a5ee5207d6192c55d5233602068b
fernelo <ferne.lo.2017@sis.smu.edu.sg>
2019-04-07 21:48:34 +0800

    Revert "fixed wrong customerid"
    This reverts commit 6d805bc394cc670fd29d83c8e1173581c1a73bfd.

--- ProjectDatabase/food.sql
+++ ProjectDatabase/food.sql
@@ -1,55 +1,31 @@
--- phpMyAdmin SQL Dump
--- version 4.7.4
--- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: Apr 06, 2019 at 10:59 AM
--- Server version: 5.7.19
--- PHP Version: 7.1.9
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
+-- Database: `foods`
 --
--- Database: `food`
---
-
--- --------------------------------------------------------
+DROP DATABASE IF EXISTS `food`;
+CREATE DATABASE `food`;
+USE `food`;
 
 --
 -- Table structure for table `food`
 --
 
 DROP TABLE IF EXISTS `food`;
-CREATE TABLE IF NOT EXISTS `food` (
+CREATE TABLE `food` (
   `food_id` int(3) NOT NULL AUTO_INCREMENT,
-  `food_name` varchar(50) DEFAULT NULL,
+  `food_name` varchar(50),
   `food_price` decimal(12,2) NOT NULL,
-  `food_description` varchar(1000) DEFAULT NULL,
+  `food_description` varchar(1000),
+  
   PRIMARY KEY (`food_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `food`
---
+);
 
 INSERT INTO `food` (`food_id`, `food_name`, `food_price`, `food_description`) VALUES
-(1, 'Apple Pie', '2.00', 'Best apple pie'),
-(2, 'Banana Pie', '4.00', 'Best banana pie'),
-(3, 'Pear Pie', '6.00', 'Best pear pie'),
-(4, 'Laksa', '8.00', 'Our housebrand Laksa'),
-(5, 'Chicken Satay', '3.50', 'Grill chicken on stick'),
-(6, 'Snake Soup', '50.00', 'The soup will make you strong like a snake. A strong snake.');
-COMMIT;
+(001, 'Apple Pie', 2.00, 'Best apple pie'),
+(002, 'Banana Pie', 4.00, 'Best banana pie'),
+(003, 'Pear Pie', 6.00, 'Best pear pie'),
+(004, 'Laksa', 8.00, 'Our housebrand Laksa'),
+(005, 'Chicken Satay', 3.50, 'Grill chicken on stick'),
+(006, 'Snake Soup', 50.00, 'The soup will make you strong like a snake. A strong snake.');
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+
